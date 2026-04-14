70c7ab83c978ae6b5794b807c8b8ad1d46b3faca
santana-rodrigo-aluno-senac-rio <santana.rodrigo@aluno.senac.rio>
2026-04-13 21:00:57 -0300

    aula08

--- ex40a.py
+++ ex40a.py
@@ -1,12 +1,6 @@
-# Usando while, desenvolva um cod em  py que some os numeros de 1 a 100
-
-
-i = 1          # Contador (começa no 1)
-soma = 0       # Acumulador (começa vazio)
-
+i=0
+n=0
 while i <= 100:
-    soma = soma + i  # Pega o que já tem na soma e adiciona o valor atual de i
-    i = i + 1        # Passa para o próximo número
-
-print("A soma de 1 até 100 é:", soma) # O resultado da soma de 1 a 100 é 5050.
-
+   n=n+1
+   i=i+1
+print(n)
---
+++ ex41a.py
@@ -0,0 +1,4 @@
+nome=""
+while nome != "sair":
+    nome = input("Digite um nome: ")
+    print(f"Olá {nome}")
