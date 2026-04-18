166e2908b0fd937edd83cbe4c71842cad489d4e8
RODRIGO BARBOSA DE SANTANA <santana.rodrigo@aluno.senac.rio>
2026-04-17 21:32:24 -0300

    aula10

---
+++ pd100.py
@@ -0,0 +1,16 @@
+#create TABLE aluno (
+#  matricula INTEGER PRIMARY KEY,
+#  nome TEXT not NULL,
+#  turma TEXT
+#  );
+
+#SELECT * FROM aluno;
+
+#INSERT INTO aluno (matricula, nome, turma)
+#VALUES (5, 'Marcela', '3° Ano B');
+
+#SELECT * FROM aluno WHERE turma = '3° Ano B';
+
+#SELECT * FROM aluno WHERE nome = 'Roberto';
+
+print(sql)
